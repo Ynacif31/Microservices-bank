5ebc3dcff08cae40a44ba0f9da05873539812fd7
Ygor Nacif <ygor.nacif00@gmail.com>
2025-01-10 16:21:01 -0300

    Account microservice implemented some functions

---
+++ accounts/src/main/java/com/ygornacif/accounts/repositories/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.ygornacif.accounts.repositories;
+
+import com.ygornacif.accounts.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+
+}
