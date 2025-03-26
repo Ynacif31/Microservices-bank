4f00c8cc3ca2a2a27711ddcad73175a9d423f29a
Ygor Nacif <ygor.nacif00@gmail.com>
2025-03-26 17:16:03 -0300

    feat: eurekaserver started

---
+++ eurekaserver/src/test/java/com/ygornacif/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygornacif.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
